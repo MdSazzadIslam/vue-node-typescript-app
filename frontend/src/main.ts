5b4a5c7a287bd343e38a694a700c7906f0ffe94d
Sazzad <netsazzad@gmail.com>
2021-10-28 00:49:42 +0600

    bootstrap css import is done

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router, BootstrapVue, IconsPlugin).mount("#app");
